766a4538d1afea75dd5d6fd86c0a75566c2b5210
SAID M. ALI <sayyidali6114@gmail.com>
2025-10-31 01:01:31 +0300

    Update Firebase configuration with project details

--- firebase-config.js
+++ firebase-config.js
@@ -1,15 +1,16 @@
-// Replace the values below with your Firebase project config and save as firebase-config.js
+
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
 const firebaseConfig = {
-  apiKey: "REPLACE_API_KEY",
-  authDomain: "REPLACE_AUTH_DOMAIN",
-  projectId: "REPLACE_PROJECT_ID",
-  storageBucket: "REPLACE_STORAGE_BUCKET",
-  messagingSenderId: "REPLACE_MESSAGING_SENDER_ID",
-  appId: "REPLACE_APP_ID"
+  apiKey: "AIzaSyBOXlrV2E8EUUZl5uMYDCdMWy_UXEnht88",
+  authDomain: "afya-vault-c8372.firebaseapp.com",
+  projectId: "afya-vault-c8372",
+  storageBucket: "afya-vault-c8372.firebasestorage.app",
+  messagingSenderId: "295891002235",
+  appId: "1:295891002235:web:6c5d66b918ab54aaa513de",
+  measurementId: "G-W6HPJ1D5N4"
 };
-if(Object.keys(firebaseConfig).length) {
-  firebase.initializeApp(firebaseConfig);
-  console.log('Firebase ready');
-} else {
-  console.warn('Firebase config missing - please configure firebase-config.js');
-}
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
